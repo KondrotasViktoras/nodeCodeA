d8ea0d523e65ae767e3c40fea1838bd77fe21979
Viktoras Kondrotas <viktoras.kondrotas@gmail.com>
2023-09-12 19:51:36 +0300

    asdasd

---
+++ Paskaita 1/server.js
@@ -0,0 +1,14 @@
+import { log } from "console";
+import express from "express";
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello World!",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on PORT 3000");
+});
